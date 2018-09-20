7573c59ba95121db204953a355cbe936e2c3c452
Nishant Vyas <nvyas3095@gmail.com>
2018-09-20 14:21:09 -0700

    Added a form for the /articles/add page. Ablility to enter a new article and have it appear on the home page. Also interacts with the articles collection in the nodekb database for MongoDB and inserts the new article and its information into the articles collection.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // The below allows us to connect to our database
 mongoose.connect('mongodb://localhost/nodekb');
@@ -25,6 +26,13 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Middleware for body-parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 // The below handles a GET request for the home page ('/') (The home route)
 app.get('/', function(req, res){
   // The below is passing the 'articles' collection from the nodekb database (using the article.js file)
@@ -46,6 +54,27 @@ app.get('/', function(req, res){
 app.get('/articles/add', function(req, res){
   res.render('add_article', {
     title:'Add Article'
+  });
+});
+
+// Add Submit POST route
+app.post('/articles/add', function(req, res){
+  // Below we create a new article variable
+  let article = new Article();
+  // Now we get the form values from the /articles/add page (add_article.pug) and put it into the articles collection fields
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+  // Below we save these fields and insert it into the collection
+  article.save(function(err){
+    // If there is an error, output the error
+    if(err){
+      console.log(err);
+      return;
+      // If not, redirect to the home page
+    } else {
+      res.redirect('/');
+    }
   });
 });
 
