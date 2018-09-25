a5e0b591087b9f88c37c683154af30f1a27380d9
Nishant Vyas <nvyas3095@gmail.com>
2018-09-25 14:42:38 -0700

    Created some access control. Created a ensureAuthenticated function which checks if the user is authenticeated. This is possible with passport. If they are not authenticated, there will be a flash message to login and will redirect you to the login page. This function was added to the loading an edit form route to make sure only users that made that specific article can edit the article and no one else can access that edit article form through the url. Also added to the adding articles route. The edit and delete buttons now only show is the user id of that article matches the id of the logged in user. The Add Article and Logout options in the navbar only show if the user exists.

--- routes/articles.js
+++ routes/articles.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 // Bring in the models pages (route for the models)
 let Article = require('../models/article');
+let User = require('../models/user');
 
 // The below is a route for adding articles
-router.get('/add', function(req, res){
+router.get('/add', ensureAuthenticated, function(req, res){
   res.render('add_article', {
     title:'Add Article'
   });
@@ -15,7 +16,7 @@ router.get('/add', function(req, res){
 router.post('/add', function(req, res){
   // Below we are requesting to check the input with the name 'title' with the message followed after with the rule 'notEmpty' followed after
   req.checkBody('title', 'Title is required').notEmpty();
-  req.checkBody('author', 'Author is required').notEmpty();
+  // req.checkBody('author', 'Author is required').notEmpty();
   req.checkBody('body', 'Body is required').notEmpty();
 
   // Get Errors
@@ -31,7 +32,8 @@ router.post('/add', function(req, res){
     let article = new Article();
     // Now we get the form values from the /articles/add page (add_article.pug) and put it into the articles collection fields
     article.title = req.body.title;
-    article.author = req.body.author;
+    // Instead of an arbitrary author, we will store the id of the user
+    article.author = req.user._id;
     article.body = req.body.body;
     // Below we save these fields and insert it into the collection
     article.save(function(err){
@@ -50,15 +52,21 @@ router.post('/add', function(req, res){
 });
 
 // The below is a route for loading the Edit form
-router.get('/edit/:id', function(req, res){
+router.get('/edit/:id', ensureAuthenticated, function(req, res){
   // We use 'Article' here since we are using the model (schema we created)
   // To get id that is in the url when we click an article link, we use req.params.id
   Article.findById(req.params.id, function(err, article){
-    // Upon clicking the 'Edit' button, it will render the 'article.pug' file
-    res.render('edit_article', {
-      title:'Edit Article',
-      article:article
-    });
+    // If the id of the author of the article does not match the id of the user that is currently logged in, send a message and redirect to the home page
+    if(article.author != req.user._id){
+      req.flash('danger', 'Not Authorized');
+      res.redirect('/');
+    } else {
+      // Upon clicking the 'Edit' button, it will render the 'article.pug' file
+      res.render('edit_article', {
+        title:'Edit Article',
+        article:article
+      });
+    }
   });
 });
 
@@ -89,14 +97,26 @@ router.post('/edit/:id', function(req, res){
 
 // The below is a route for deleting articles
 router.delete('/:id', function(req, res){
+  if(!req.user._id){
+    res.status(500).send();
+  }
   // Below we create a query where the '_id' is equal to the ':id' we requested from above
   let query = {_id:req.params.id}
-  // Then, using the 'Article' schema, we remove the article based on the query
-  Article.remove(query, function(err){
-    if(err){
-      console.log(err);
+  // We use the findById method and pass in the id from the url with a function
+  Article.findById(req.params.id, function(err, article){
+    // If the article author id is not the same as the user's id thats logged in, send a 500 error
+    if(article.author != req.user._id){
+      res.status(500).send();
+      // Else we can remove
+    } else {
+      // Then, using the 'Article' schema, we remove the article based on the query
+      Article.remove(query, function(err){
+        if(err){
+          console.log(err);
+        }
+        res.send('Success');
+      });
     }
-    res.send('Success')
   });
 });
 
@@ -106,11 +126,29 @@ router.get('/:id', function(req, res){
   // We use 'Article' here since we are using the model (schema we created)
   // To get id that is in the url when we click an article link, we use req.params.id
   Article.findById(req.params.id, function(err, article){
-    // Upon clicking the specific article, it will render the 'article.pug' file
-    res.render('article', {
-      article:article
+    // We use the findById method from User schema and pass in article.author since thats the variable that holds the id
+    User.findById(article.author, function(err, user){
+      // Upon clicking the specific article, it will render the 'article.pug' file
+      res.render('article', {
+        article:article,
+        author: user.name
+      });
     });
   });
 });
+
+// Access Control
+// We can pass in this function to any routes above that we want to protect
+// Once passed in, users can no longer acces the page directly from the URL
+function ensureAuthenticated(req, res, next){
+  // If the request is authenticated, then we can move on (possible with passport)
+  if(req.isAuthenticated()){
+    return next();
+    // Else we send a message and redirect
+  } else {
+    req.flash('danger', 'Please login');
+    res.redirect('/users/login');
+  }
+}
 
 module.exports = router;
